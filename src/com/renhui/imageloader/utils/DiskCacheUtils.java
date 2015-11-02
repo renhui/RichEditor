dd763463a02a3d601f226fb2773df6d2ed398ae1
renhui <1418757529@qq.com>
2015-11-02 19:19:04 +0800

    UIL --- session 1

---
+++ src/com/renhui/imageloader/utils/DiskCacheUtils.java
@@ -0,0 +1,18 @@
+package com.renhui.imageloader.utils;
+
+import java.io.File;
+
+/**
+ * 图片磁盘缓存工具类
+ * 
+ * @author renhui
+ */
+public class DiskCacheUtils {
+
+	// 私有构造函数
+	private DiskCacheUtils() {
+	}
+	
+	//TODO 
+
+}
