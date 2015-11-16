d294b0f1ddb9f5237ff3f5936c758a49636ba944
renhui <1418757529@qq.com>
2015-11-16 19:13:46 +0800

    数据库问题修复

--- src/com/example/richtext/sqlite/DatabaseHelper.java
+++ src/com/example/richtext/sqlite/DatabaseHelper.java
@@ -1,84 +1,17 @@
 package com.example.richtext.sqlite;
 
-import java.io.File;
-
 import com.example.richtext.sqlite.tables.SqliteV1;
-import com.example.richtext.utils.DebugTraceTool;
-
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteDatabase.CursorFactory;
 import android.database.sqlite.SQLiteOpenHelper;
 
-/**
- * 数据库适配器
- * 
- * @author renhui
- */
 public class DatabaseHelper extends SQLiteOpenHelper {
 
-	
-
-	//	private static final String mDbFolder = "richtext_sqlite";
-	private static final String mDbName = "richtext.db";
-	private static final int mDbVersion = 1;
-
-	private SQLiteDatabase mDatabase;
-
-	
 	public DatabaseHelper(Context context, String name, CursorFactory factory, int version) {
 		super(context, name, factory, version);
 	}
 	
-	public DatabaseHelper(Context context) {
-		this(context, mDbName, null, mDbVersion);
-	}
-//		super(context, mDbName, null, mDbVersion);
-//
-//		String pathToSqlite = context.getDatabasePath(mDbFolder).getPath();
-//		DebugTraceTool.debugTraceE(this, "Dir:" + pathToSqlite);
-//
-//		File sqliteDir = new File(pathToSqlite);
-//
-//		if (!sqliteDir.exists() || !sqliteDir.isDirectory()) {
-//			sqliteDir.mkdirs();
-//		}
-//
-//		new File(sqliteDir + File.separator + mDbName);
-//
-//		DebugTraceTool.debugTrace(this, "DB Path:" + pathToSqlite
-//				+ File.separator + mDbName);
-//
-//		mDatabase = SQLiteDatabase.openDatabase(pathToSqlite + File.separator
-//				+ mDbName, null, SQLiteDatabase.CREATE_IF_NECESSARY | SQLiteDatabase.OPEN_READWRITE);
-//
-//		if (mDatabase != null) {
-//			int version = mDatabase.getVersion();
-//
-//			DebugTraceTool.debugTrace(this, "Current Version:" + mDbVersion
-//					+ ", DB Version:" + version);
-//
-//			if (version != mDbVersion) {
-//				mDatabase.beginTransaction();
-//
-//				try {
-//					if (version == 0) {
-//						onCreate(mDatabase);
-//					} else {
-//						onUpgrade(mDatabase, version, mDbVersion);
-//					}
-//					
-//					mDatabase.setVersion(mDbVersion);
-//					mDatabase.setTransactionSuccessful();
-//				} finally {
-//					mDatabase.endTransaction();
-//				}
-//			}
-//		} else {
-//			DebugTraceTool.debugTrace(this, "Create Database error!");
-//		}
-//	}
-
 	@Override
 	public void onCreate(SQLiteDatabase database) {
 		new SqliteV1().onCreate(database);
@@ -93,9 +26,5 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 //		default:
 //			break;
 //		}
-	}
-	
-	public SQLiteDatabase getDatabase() {
-		return mDatabase;
 	}
 }
--- src/com/example/richtext/ui/activity/NoteActivity.java
+++ src/com/example/richtext/ui/activity/NoteActivity.java
@@ -6,6 +6,7 @@ import com.example.richtext.R;
 import com.example.richtext.moudle.Note;
 import com.example.richtext.sqlite.DatabaseAccessFactory;
 import com.example.richtext.ui.adapter.NoteAdapter;
+import com.example.richtext.utils.DebugTraceTool;
 
 import android.os.Bundle;
 import android.widget.ListView;
@@ -23,10 +24,8 @@ public class NoteActivity extends BaseActivity {
 		mListView  = (ListView) findViewById(R.id.note_list);
 
 		ArrayList<Note> noteList = DatabaseAccessFactory.getInstance(this).noteAccessor().getNotes();
+		DebugTraceTool.debugTraceE(this, "数量"+ noteList.size());
 		mAdapter = new NoteAdapter(this, noteList);
 		mListView.setAdapter(mAdapter);
 	}
-
-	
-
 }
