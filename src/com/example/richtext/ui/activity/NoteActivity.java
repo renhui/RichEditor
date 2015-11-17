15662884c2108984b9a1a8c1e30c330842ca5e05
renhui <1418757529@qq.com>
2015-11-17 18:06:27 +0800

    增加Fab

--- src/com/example/richtext/ui/activity/NoteActivity.java
+++ src/com/example/richtext/ui/activity/NoteActivity.java
@@ -6,10 +6,17 @@ import com.example.richtext.R;
 import com.example.richtext.moudle.Note;
 import com.example.richtext.sqlite.DatabaseAccessFactory;
 import com.example.richtext.ui.adapter.NoteAdapter;
+import com.example.richtext.ui.widget.fab.FloatingActionButton;
+import com.example.richtext.ui.widget.fab.FloatingActionsMenu;
 import com.example.richtext.utils.DebugTraceTool;
 
+import android.graphics.drawable.ShapeDrawable;
+import android.graphics.drawable.shapes.OvalShape;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.ListView;
+import android.widget.Toast;
 
 public class NoteActivity extends BaseActivity {
 
@@ -19,13 +26,42 @@ public class NoteActivity extends BaseActivity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		
-		setContentView(R.layout.acitivity_note);
-		mListView  = (ListView) findViewById(R.id.note_list);
 
-		ArrayList<Note> noteList = DatabaseAccessFactory.getInstance(this).noteAccessor().getNotes();
-		DebugTraceTool.debugTraceE(this, "数量"+ noteList.size());
+		setContentView(R.layout.acitivity_note);
+		mListView = (ListView) findViewById(R.id.note_list);
+
+		ArrayList<Note> noteList = DatabaseAccessFactory.getInstance(this)
+				.noteAccessor().getNotes();
+		DebugTraceTool.debugTraceE(this, "数量" + noteList.size());
 		mAdapter = new NoteAdapter(this, noteList);
 		mListView.setAdapter(mAdapter);
+
+		final View actionB = findViewById(R.id.action_b);
+
+		FloatingActionButton actionC = new FloatingActionButton(
+				getBaseContext());
+		actionC.setTitle("Hide/Show Action above");
+		actionC.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				actionB.setVisibility(actionB.getVisibility() == View.GONE ? View.VISIBLE
+						: View.GONE);
+			}
+		});
+
+		final FloatingActionsMenu menuMultipleActions = (FloatingActionsMenu) findViewById(R.id.multiple_actions);
+		menuMultipleActions.addButton(actionC);
+
+		ShapeDrawable drawable = new ShapeDrawable(new OvalShape());
+		drawable.getPaint().setColor(getResources().getColor(R.color.white));
+
+		final FloatingActionButton actionA = (FloatingActionButton) findViewById(R.id.action_a);
+		actionA.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				actionA.setTitle("Action A clicked");
+			}
+		});
+
 	}
 }
